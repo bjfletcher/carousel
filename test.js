e57a8a3b9822fb35a07226f251d1a21c0da6fff2
Ben Fletcher <bjfletcher@gmail.com>
2014-09-05 13:28:26 +0100

    BF: old carousel code

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 describe("carousel", function() {
 
     it("should be define", function() {
-
         expect(Carousel).toBeDefined();
-
     });
+
+    it("should have buttons", function() {
+        document.body.innerHTML = __html__['test.html'];
+        expect(document.getElementById('xtitle-avengers1')).toBeDefined();
+    });
+
 });
