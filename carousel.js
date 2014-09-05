98a3e2d268e806cf00f9896c2842105352cd0ece
Ben Fletcher <ben@Bens-MacBook-Pro.local>
2014-09-05 23:24:36 +0200

    BF: trying some old test

--- carousel.js
+++ carousel.js
@@ -1,9 +1,15 @@
 var Carousel = function(el) {
-  this.el = el;
+	this.el = el;
 };
 
 Carousel.prototype = {
-  constructor: Carousel,
-  init: function() {
-  }
+	constructor: Carousel,
+	init: function() {
+		var slide = this.el.querySelector('.skycom-carousel-container > .slide:nth-child(1)');
+		if (slide.classList) {
+			slide.classList.add('active');
+		} else {
+			slide.className += ' active';
+		}
+	}
 };
--- test.js
+++ test.js
@@ -4,9 +4,16 @@ describe("carousel", function() {
         expect(Carousel).toBeDefined();
     });
 
-    it("should have buttons", function() {
-        document.body.innerHTML = __html__['test.html'];
-        expect(document.getElementById('xtitle-avengers1')).toBeDefined();
-    });
+	it("should have an active slide", function() {
+		document.body.innerHTML = __html__['test.html'];
+
+		var el = document.querySelector('#hero');
+		var carousel = new Carousel(el);
+		carousel.init();
+
+		var activeSlide = el.querySelector('.skycom-carousel-container > .active');
+
+		expect(activeSlide).not.toBeNull();
+	});
 
 });
